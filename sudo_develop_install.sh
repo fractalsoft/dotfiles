c212668a97c2c92355a75707aaf313af4541beeb
Alek Malaszkiewicz <torrocus@users.noreply.github.com>
2015-02-11 08:30:50 +0100

    Update sudo_develop_install.sh

--- sudo_develop_install.sh
+++ sudo_develop_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:webupd8team/atom
 sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome-beta.list'
 sudo aptitude update
 sudo aptitude install \
+  atom \
   firefox \
   google-chrome-beta \
   opera \
