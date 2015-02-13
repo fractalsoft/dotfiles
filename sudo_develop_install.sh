98a1ce5bc1095828e4ea7dbe0fe194ca307b2cf1
Alek Malaszkiewicz <torrocus@users.noreply.github.com>
2015-02-13 10:04:57 +0100

    Update sudo_develop_install.sh

--- sudo_develop_install.sh
+++ sudo_develop_install.sh
@@ -6,6 +6,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome-beta.list'
 sudo aptitude update
 sudo aptitude install \
+  android-tools-adb \
   atom \
   firefox \
   google-chrome-beta \
