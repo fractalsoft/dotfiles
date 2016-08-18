a56a3fb0f627133de83053b87906b01310fdede3
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2016-08-18 15:45:44 +0200

    Update ruby gems

--- ruby/gems.bash
+++ ruby/gems.bash
@@ -1,2 +1,2 @@
-gem install bundler rubocop rubocop-git rails_best_practices brakeman sandi_meter csscss rubycritic reek roodi flog flay git-up bundle-audit scss-lint lolcat
+gem install bundler rubocop rubocop-git rails_best_practices brakeman sandi_meter csscss rubycritic reek roodi flog flay git-up bundler-audit scss-lint lolcat mailcatcher
 
